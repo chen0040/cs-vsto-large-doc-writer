403a6f9f69177cd4ea126da7bb1ba8cde9ea7230
xschen <xs0040@gmail.com>
2017-06-02 14:15:11 +0800

    project setup

---
+++ cs-vsto-large-doc-writer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_vsto_large_doc_writer
+{
+    public class Class1
+    {
+    }
+}
